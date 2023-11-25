c2113131c83f1d0c3f89dd254f6c90bb799879cc
andysign <andysignerdesigner@gmail.com>
2023-11-25 15:36:27 +0000

    Changed to port 8080 and added swagger module avail at .../api

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('API')
+      .setDescription('The Backend API')
+      .setVersion('1.0')
+      .addTag('backend')
+      .build(),
+  );
+  SwaggerModule.setup('api', app, document);
   await app.listen(8080);
 }
 bootstrap();
