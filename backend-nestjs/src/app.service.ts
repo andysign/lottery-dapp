7ac9a241986b382a67df1e75bd1cde1b0e480e16
andysign <andysignerdesigner@gmail.com>
2023-11-26 01:40:26 +0000

    Added /block-number

--- backend-nestjs/src/app.service.ts
+++ backend-nestjs/src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+import { ethers } from 'ethers';
+
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class AppService {
+  private provider: ethers.Provider;
+
+  constructor(private configService: ConfigService) {
+    this.provider = new ethers.JsonRpcProvider(
+      this.configService.get<string>(
+        'RPC_ENDPOINT_URL',
+        process.env.RPC_ENDPOINT_URL,
+      ),
+    );
+  }
+
   getHello(): string {
     return 'Backend App Running OK. Go to .../api/ for more!';
+  }
+
+  async getBlockNumber() {
+    const { provider } = this;
+    const blkNum = await provider.getBlockNumber();
+    const lastBlkNum = blkNum | 0;
+    return lastBlkNum;
   }
 }
